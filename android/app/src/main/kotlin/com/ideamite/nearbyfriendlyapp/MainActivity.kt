bca513d819e119c0424367e0531e2b550694cd0a
jarias14 <jarias14@gmail.com>
2018-10-26 07:14:13 -0400

    Initial commit

---
+++ android/app/src/main/kotlin/com/ideamite/nearbyfriendlyapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ideamite.nearbyfriendlyapp
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity(): FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
